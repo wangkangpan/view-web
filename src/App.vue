a60130c951e7ec2ec6d3a50c80a3fd3eebefbedf
wangkangpan <WKPSDL@outlook.com>
2022-06-05 10:55:14 +0800

    fix: 解决版本不兼容问题

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
-<script>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
-import Loader from "./components/loader.vue"
-export default {
-  components: {Loader}
-}
-
-</script>
-
-<!--<template>-->
-<!--  <img alt="Vue logo" src="./assets/logo.png" />-->
-<!--  <HelloWorld msg="Hello Vue 3 + Vite" />-->
-<!--</template>-->
 <template>
   <Loader></Loader>
 </template>
+
+
+<script>
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  // import HelloWorld from './components/HelloWorld.vue'
+  import Loader from "./components/loader.vue"
+  export default {
+    components: { Loader }
+  }
+</script>
 
 
--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,25 +1,44 @@
 <template>
-    <div>
-        666
-        <form>
-            <el-input id = "account" placeholder="请输入账号"></el-input>
-            <el-input id = "password" placeholder="请输入密码"></el-input>
-        </form>
+    <div class = "loader">
+            <el-form class = "loader-form" :size="size">
+                <el-space wrap>
+                    <el-form-item>
+                            <el-input id = "account" placeholder="请输入账号"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                            <el-input id = "password" placeholder="请输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                            <el-button>登录</el-button>
+                    </el-form-item>
+                    <el-form-item>
+                            <el-button>注册</el-button>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-link type="warning">忘记密码,去重置</el-link>
+                    </el-form-item>
+                </el-space>
+            </el-form>
     </div>
 </template>
 
 <script>
-    import { ElInput } from 'element-plus'
+    import { ref } from 'vue'
+    const size = ref('default')
 
     export default {
         name : 'Loader',
-        components: {
-            ElInput
-        }
+        size
     }
-
 </script>
 <!-- scoped样式私有化-->
 <style scoped>
-
+    .loader{
+        /*width: 400px;*/
+        /*height: 400px;*/
+        background-color: #ffffff;
+    }
+    .loader-form{
+        color: #42b983;
+    }
 </style>
